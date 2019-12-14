fac35d238c20919c97e3cf4f2d860beef1202c5a
Graham Hoyes <accounts@grahamhoyes.com>
2019-12-14 14:15:01 -0500

    Black formatting

--- application/__init__.py
+++ application/__init__.py
@@ -14,6 +14,7 @@ from flask_mail import Mail
 
 # Initialize pymysql for mysql support in deployment
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 import os
@@ -28,23 +29,24 @@ migrate = Migrate()
 login_manager = LoginManager()
 
 # Initialize the mail instance
-#NOTE IF MAIL GIVES A RECURSION ERROR YOU MAY HAVE INSTALLED THE NEW FLASK MAIL VERSION WHICH IS CURRENTLY BUGGED
-#TRY ROLLING BACK TO flask_mail version 0.9.0 instructions can be found in README
+# NOTE IF MAIL GIVES A RECURSION ERROR YOU MAY HAVE INSTALLED THE NEW FLASK MAIL VERSION WHICH IS CURRENTLY BUGGED
+# TRY ROLLING BACK TO flask_mail version 0.9.0 instructions can be found in README
 mail = Mail()
 
 """
  Encapsulate the app in a function in order to be able to initialize it with
  various environment variables for  testing as well as versatility
 """
+
+
 def create_app():
     # Define the application object
 
-
     # Change to production configuration if in production
-    if(os.environ['ENVIRONMENT'] == 'PRODUCTION'):
-        #flask_app = Flask(__name__, static_url_path = '/makeuoft/static')
+    if os.environ["ENVIRONMENT"] == "PRODUCTION":
+        # flask_app = Flask(__name__, static_url_path = '/makeuoft/static')
         flask_app = Flask(__name__)
-        config_class=ProductionConfig()
+        config_class = ProductionConfig()
         # Line for mounting the app to the /makeuoft domain
         """
         Note: ensure that ProxyPass and ReverseProxyPass are as follows on apache config:
@@ -53,13 +55,11 @@ def create_app():
         ProxyPassReverse /makeuoft http://ieee.utoronto.ca/makeuoft
         Alias /makeuoft/static /var/www/makeuoft/public_html/static
         """
-        flask_app.wsgi_app = ReverseProxied(flask_app.wsgi_app, script_name='/makeuoft')
+        flask_app.wsgi_app = ReverseProxied(flask_app.wsgi_app, script_name="/makeuoft")
 
     else:
         flask_app = Flask(__name__)
-        config_class=DevelopmentConfig()
-
-
+        config_class = DevelopmentConfig()
 
     # Configurations taken from function argument
     flask_app.config.from_object(config_class)
@@ -71,18 +71,16 @@ def create_app():
 
     # Create a LoginManager instance
     login_manager.init_app(flask_app)
-    login_manager.login_view = 'auth.login'
-    login_manager.login_message= ''
-
+    login_manager.login_view = "auth.login"
+    login_manager.login_message = ""
 
     # Sample HTTP error handling
     @flask_app.errorhandler(404)
     def not_found(error):
         return render_template("404.html"), 404
 
-
     # Import a module / component using its blueprint handler variable (mod_auth)
-    #from application.mod_auth.controllers import mod_auth as auth_module
+    # from application.mod_auth.controllers import mod_auth as auth_module
     from application.home import home as home_module
     from application.auth import auth as auth_module
 
@@ -92,8 +90,8 @@ def create_app():
     flask_app.register_blueprint(home_module)
     flask_app.register_blueprint(auth_module)
 
-
     return flask_app
+
 
 from application import db_models
 
