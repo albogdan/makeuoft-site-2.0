d87bd0480361fb7b9026b015efdef591d9ea0638
Alex Bogdan <stefanalexb@gmail.com>
2019-12-16 19:23:31 -0500

    Updated suburl for hardware site

--- application/templates/hardware_signout/service-worker.js
+++ application/templates/hardware_signout/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ac4c38e3ea728b8469b7ecf94683717.js"
+  "/makeuoft/precache-manifest.d1110ef7660e8ea7db61974b641b974f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/makeuoft/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
